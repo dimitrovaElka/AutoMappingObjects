0838f55dae511110854ee9589421b6fb4d8655f6
Elka Dimitrova <dimitrova_elka@abv.bg>
2017-12-02 22:51:31 +0200

    Delete Configuration.cs

--- Employees.Data/Configuration.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace Employees.Data
-{
-    public class Configuration
-    {
-        public static string ConnectionString =>
-            @"Server=.;Database=EmployeesDb;Integrated Security=True";
-    }
-}
